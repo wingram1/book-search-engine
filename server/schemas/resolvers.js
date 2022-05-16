1d55fed9efb9e7d98c80fedcc68fef082f95fe27
wingram1 <waingram96@gmail.com>
2022-05-15 20:55:52 -0500

    authentication, login, addUser, me

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,6 +4,17 @@ const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({})
+        //   .select("-__v password")
+          .populate("books");
+
+        return userData;
+      }
+
+      throw newAuthenticationError("Not logged in");
+    },
     users: async () => {
       return User.find().select("-__v -password").populate("books");
     },
@@ -38,6 +49,19 @@ const resolvers = {
 
       const token = signToken(user);
       return { token, user };
+    },
+    saveBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { books: bookId } },
+          { new: true }
+        ).populate("books");
+
+        return updatedUser;
+      }
+
+      throw new AuthenticationError("You need to be logged in!");
     },
   },
 };
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const { ApolloServer } = require("apollo-server-express");
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
 const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 
 const app = express();
